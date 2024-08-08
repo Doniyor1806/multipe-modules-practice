5c04d55978fcecfd73a70defed99b6e4a35c0592
Doniyor1806 <mydonis23@gmail.com>
2024-08-07 21:36:26 -0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "aws_iam_user" "users" {
+  for_each = toset(var.users)
+  name     = each.key
+  path     = "/"
+}
+variable "users" {
+  type = list(string)
+}
+
+output "user_name" {
+  value = [for user in aws_iam_user.users : user.name]
+}
